fbe0f7501a0da3dd0b3d2a2d5d4d8755605d756b
adhikarimanshi <adhikarimanshi03@gmail.com>
2024-05-30 22:28:15 +0545

    Sample Project

---
+++ WebApp1ByManshi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp1byManshi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
